1951af8d3e8462f9a6153c2985ea33164959b90f
Hasegawa Yohei <yoheybrains@gmail.com>
2018-08-17 21:17:09 +0900

    added: setup script

---
+++ scripts/createDatabase.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('postgres', process.env.DATABASE_USER, process.env.DATABASE_PASSWORD, {
+  operatorsAliases: false,
+  dialect: 'postgres'
+});
+
+sequelize
+  .query(`CREATE DATABASE ${process.env.DATABASE_NAME};`)
+  .then(() => {
+    console.log(`SUCCESS: CREATE DATABASE ${process.env.DATABASE_NAME}.`);
+  })
+  .catch(error => {
+    if (error.original.code === '42P04') console.error(`ERROR! database ${process.env.DATABASE_NAME} already exists.`);
+    else console.error(error);
+  })
+  .finally(() => {
+    sequelize.close();
+  });
