1d7499c30342b4583635d1a29c852a89baf1b379
Hasegawa Yohei <yoheybrains@gmail.com>
2018-09-10 23:31:51 +0900

    modified: add user info to db

--- skills/team_join.js
+++ skills/team_join.js
@@ -1,13 +1,34 @@
 const TeamJoinService = new (require('../worker_service/TeamJoinService.js'))();
+const models = require('../models');
+const { promisify } = require('util');
 
 module.exports = controller => {
-  controller.on('team_join', (bot, message) => {
+  controller.on('team_join', async (bot, message) => {
     bot.reply(message, 'Welcome, <@' + message.user + '>');
 
     const welcomeText = TeamJoinService.getWelcomeText();
     bot.say({
       text: welcomeText,
       channel: `${message.user}`
+    });
+
+
+    const user = await promisify(bot.api.users.info)({
+      token: process.env.AUTH_TOKEN,
+      user: message.user
+    }).catch(err => {
+      console.error(err);
+      return {};
+    });
+
+    const today = new Date();
+    models.user.create({
+      name: user.user.real_name,
+      slackId: user.user.id,
+      enrolledYear: today.getFullYear(),
+      disable: false,
+      created_at: today,
+      updated_at: today
     });
   });
 };
